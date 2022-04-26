7a0eaf3aff5a94bdc795afd9bde96da5f195a0a4
Eder <lg2xspeed@googlemail.com>
2022-04-26 21:13:15 +0200

    Create authModule and initial basic auth guard

---
+++ src/modules/auth/auth-basic.strategy.ts
@@ -0,0 +1,21 @@
+import { BasicStrategy as Strategy } from 'passport-http';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class BasicStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      passReqToCallback: true,
+    });
+  }
+
+  public validate = async (req, username, password): Promise<boolean> => {
+    //todo get data from config
+    if ('test' === username && 'password' === password) {
+      return true;
+    }
+    throw new UnauthorizedException();
+  };
+}
