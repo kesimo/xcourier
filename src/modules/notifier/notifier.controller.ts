de18d9009bcc31916af8761979fa0daef72c1c0d
Eder <lg2xspeed@googlemail.com>
2022-04-28 13:02:36 +0200

    controller cleanup and route completition

--- src/modules/notifier/notifier.controller.ts
+++ src/modules/notifier/notifier.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Get,
   NotFoundException,
-  NotImplementedException,
   Param,
   Post,
   Put,
@@ -19,22 +18,58 @@ import { NotifierService } from './notifier.service';
 export class NotifierController {
   constructor(private readonly notifierService: NotifierService) {}
 
+  /**
+   * Default GET Entrypoint
+   *
+   * @param {*} query input parameters (id required) -> Query only, body ignored
+   * @return {*}  {Promise<IResponseStatus>} action result containing status and count of sent mails
+   * @memberof NotifierController
+   */
   @Get('/params')
   @UseGuards(AuthGuard('basic'))
-  async sendNotificationByGetQuery(@Query() query) {
+  async sendNotificationByGetQuery(@Query() query): Promise<IResponseStatus> {
     if (!query.id) throw new NotFoundException('Endpoint id is missing');
-    const status = this.notifierService.sendMailNotification(query.id, query);
+    const cleaned_query = query;
+    delete cleaned_query.id;
+    const status = this.notifierService.sendMailNotification(
+      query.id,
+      cleaned_query,
+    );
     return status;
   }
 
+  /**
+   * Default POST Entrypoint
+   *
+   * @param {*} query input parameters (id required) -> query only, body ignored
+   * @return {*}  {Promise<IResponseStatus>} action result containing status and count of sent mails
+   * @memberof NotifierController
+   */
   @Post('/params')
   @UseGuards(AuthGuard('basic'))
-  async sendNotificationByPostQuery(@Query() query: any) {
+  async sendNotificationByPostQuery(
+    @Query() query: any,
+  ): Promise<IResponseStatus> {
     if (!query.id) throw new NotFoundException('Endpoint id is missing');
-    const status = this.notifierService.sendMailNotification(query.id, query);
+    const cleaned_query = query;
+    delete cleaned_query.id;
+    const status = this.notifierService.sendMailNotification(
+      query.id,
+      cleaned_query,
+    );
     return status;
   }
 
+  /**
+   * Extended GET Entrypoint
+   *
+   * @param {string} id id of defined entrypoint (configured in yaml)
+   * @param {Express.Request} req
+   * @param {*} body json body with notification data
+   * @param {*} query input parameters (id required)
+   * @return {*}  {Promise<IResponseStatus>} action result containing status and count of sent mails
+   * @memberof NotifierController
+   */
   @Get('/:customId')
   @UseGuards(AuthGuard('basic'))
   async sendNotificationGet(
@@ -44,11 +79,20 @@ export class NotifierController {
     @Query() query: any,
   ): Promise<IResponseStatus> {
     const data = { ...query, ...body };
-    //console.log(data);
     const status = this.notifierService.sendMailNotification(id, data);
     return status;
   }
 
+  /**
+   * Extended POST Entrypoint (Copy of GET)
+   *
+   * @param {string} id id of defined entrypoint (configured in yaml)
+   * @param {Express.Request} req
+   * @param {*} body json body with notification data
+   * @param {*} query input parameters (id required)
+   * @return {*}  {Promise<IResponseStatus>} action result containing status and count of sent mails
+   * @memberof NotifierController
+   */
   @Post('/:customId')
   async sendNotificationPush(
     @Param('customId') id: string,
@@ -56,17 +100,30 @@ export class NotifierController {
     @Body() body: any,
     @Query() query: any,
   ): Promise<IResponseStatus> {
-    const status = this.notifierService.sendMailNotification(id, body);
+    const data = { ...query, ...body };
+    const status = this.notifierService.sendMailNotification(id, data);
     return status;
   }
 
+  /**
+   * Extended PUT Entrypoint (Copy of GET)
+   *
+   * @param {string} id id of defined entrypoint (configured in yaml)
+   * @param {Express.Request} req
+   * @param {*} body json body with notification data
+   * @param {*} query input parameters (id required)
+   * @return {*}  {Promise<IResponseStatus>} action result containing status and count of sent mails
+   * @memberof NotifierController
+   */
   @Put('/:customId')
   async sendNotificationPut(
     @Param('customId') id: string,
     @Req() req: Express.Request,
     @Body() body: any,
+    @Query() query: any,
   ): Promise<IResponseStatus> {
-    const status = this.notifierService.sendMailNotification(id, body);
+    const data = { ...query, ...body };
+    const status = this.notifierService.sendMailNotification(id, data);
     return status;
   }
 }
