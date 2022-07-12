640105acd9d880f00962b59e5cd23a048c7153c2
Eder <lg2xspeed@googlemail.com>
2022-07-12 08:55:59 +0200

    added workflow for auto generate package

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/zsh
 VERSION=1.1
 
 docker build -t nestjs-demo:${VERSION} .
 
 docker run -it --rm -v $(PWD)/config:/usr/src/app/xcourier/config \
-  nestjs-demo:$VERSION #/bin/sh
+  -p 3001:3001 \
+  --name nestjs-demo \
+  -e URL_PREFIX="/api" \
+  -e PORT=3001 \
+  -e DEBUG=false -e BASIC_AUTH_USERNAME="root" \
+  -e BASIC_AUTH_PASSWORD="root" -e TZ="Europe/Berlin" \
+  nestjs-demo:$VERSION
